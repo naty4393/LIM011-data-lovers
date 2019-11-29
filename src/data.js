a7b4967133db4c39a6b0fee58ef7dfa970f3be99
Naty4393 <naty4393@gmail.com>
2019-11-29 09:10:18 -0500

    update

--- src/data.js
+++ src/data.js
@@ -1,7 +1,128 @@
-/* Manejo de data */
+export const seeToChampion = (champion) => {
+  const allChampions = [];
+  for (let i = 0; i < champion.length; i += 1) {
+    allChampions.push(
+      {
+        name: champion[i].name,
+        id: champion[i].id,
+        title: champion[i].title,
+        image: champion[i].splash,
+        blurb: champion[i].blurb,
+        power: champion[i].info.attack,
+        defense: champion[i].info.defense,
+        magic: champion[i].info.magic,
+        difficulty: champion[i].info.difficulty,
+        tag1: champion[i].tags[0],
+        tag2: champion[i].tags[1],
+        levelLife: champion[i].stats.hpperlevel,
+        moveSpeed: champion[i].stats.movespeed,
+        armor: champion[i].stats.armor,
+        spellblock: champion[i].stats.spellblock,
+        stats4: champion[i].stats.attackrange,
+        stats5: champion[i].stats.attackdamage,
+        stats6: champion[i].stats.attackspeedperlevel,
+      },
+    );
+  }
+  return allChampions;
+};
 
-// esta es una función de ejemplo
+export const filterType = (data, type) => {
+  // // eslint-disable-next-line no-console
+  // console.log('data', data);
+  // // eslint-disable-next-line no-console
+  // console.log('que entra', typeof (type));
+  // // eslint-disable-next-line no-console
+  // console.log('cual es tu valor', type);
+  const filterToString = data.filter((element) => element.tag1 === type || element.tag2 === type);
+  // // eslint-disable-next-line no-console
+  // console.log(filterToString);
+  // // eslint-disable-next-line no-console
+  // console.log('salida', typeof (filterToString));
+  return filterToString;
+};
 
-export const example = () => {
-  return 'example';
+export const championsInFallingPower = (data, dato) => {
+  let filterFallingsPower;
+  if (dato === 'falling') {
+    filterFallingsPower = data.sort((a, b) => (b.power - a.power));
+  }
+  return filterFallingsPower;
+};
+
+export const championsInUpwardPower = (data, dato) => {
+  let filterUpwardsPower = [];
+  if (dato === 'upward') {
+    filterUpwardsPower = data.sort((a, b) => (a.power - b.power));
+  }
+  return filterUpwardsPower;
+};
+
+export const championsInFallingUtility = (data, dato) => {
+  let filterFallingsUtility = [];
+  if (dato === 'falling') {
+    filterFallingsUtility = data.sort((a, b) => (b.defense - a.defense));
+  }
+  return filterFallingsUtility;
+};
+
+export const championsInUpwardUtility = (data, dato) => {
+  // // eslint-disable-next-line no-console
+  // console.log('data que ingresa', data);
+  // // eslint-disable-next-line no-console
+  // console.log('dato que ingresa', dato);
+  let filterUpwardsUtility = [];
+  if (dato === 'upward') {
+    // // eslint-disable-next-line no-console
+    // console.log('ingresa');
+    filterUpwardsUtility = data.sort((a, b) => (a.defense - b.defense));
+  }
+  // eslint-disable-next-line no-console
+  // console.log('resultado', filterUpwardsUtility);
+  return filterUpwardsUtility;
+};
+
+export const championsInFallingDifficult = (data, dato) => {
+  let filterFallingsDifficult = [];
+  if (dato === 'falling') {
+    filterFallingsDifficult = data.sort((a, b) => (b.difficulty - a.difficulty));
+  }
+  return filterFallingsDifficult;
+};
+
+export const championsInUpwardDifficult = (data, dato) => {
+  let filterUpwardsDifficult = [];
+  if (dato === 'upward') {
+    filterUpwardsDifficult = data.sort((a, b) => (a.difficulty - b.difficulty));
+  }
+  return filterUpwardsDifficult;
+};
+
+export const searchWord = (data, text) => {
+  // console.log('data', data);
+  // console.log('text', text);
+  // console.log(typeof (text));
+  let enterText = '';
+  const allFindChampion = [];
+  enterText = text.toLowerCase();
+  // console.log('text en minuscula de data', enterText);
+  // console.log(typeof (enterText));
+  for (let a = 0; a < data.length; a += 1) {
+    let name = '';
+    name = data[a].name.toLowerCase();
+    // console.log('nombre', name);
+    // console.log('prueba3.9', name.startsWith(enterText));
+    if (name.startsWith(enterText) === true) {
+      // console.log('prueba4', name.startsWith(enterText));
+      allFindChampion.push(data[a]);
+      // debugger
+      // console.log('entre a un campeon mas', allFindChampion);
+    }
+    // debugger
+    // console.log('entre a un campeon mas1', allFindChampion);
+    // return allFindChampion;
+    // debugger
+  }
+  // console.log('entre a un campeon mas2', allFindChampion);
+  return allFindChampion;
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,128 @@
-/**
+/* *
  * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
+ import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
+import LoL from './data/lol/lol.js';
 
-import { example } from './data.js';
+import {
+  // eslint-disable-next-line max-len
+  seeToChampion, filterType, championsInFallingPower, championsInUpwardPower, championsInUpwardUtility,
+  championsInUpwardDifficult, searchWord, championsInFallingDifficult, championsInFallingUtility,
+} from './data.js';
 
-console.log(example);
+const type = document.querySelector('#type');
+const powers = document.querySelector('#powers');
+const utility = document.querySelector('#utility');
+const weakness = document.querySelector('#weakness');
+const textToSearch = document.querySelector('#searchTexto');
+const root = document.querySelector('#root');
+// const miModal = document.querySelector('#miModal');
+const lolData = Object.values(LoL.data);
+const newData = seeToChampion(lolData);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+const roamData = (toShowFigure) => {
+  let valuesPlayer = '';
+  toShowFigure.forEach((elementData) => {
+    valuesPlayer += `<div name="LoL" id=${elementData.id} class="contenedor">
+            <img src="${elementData.image}" id="imagenRoot" class="imagenRoot">
+            <p  name="obtener">${elementData.name}</p>
+            </div>`;
+  });
+  return valuesPlayer;
+};
+
+root.innerHTML = roamData(newData);
+
+document.querySelector('.contenedor').addEventListener('click', () => {
+  // eslint-disable-next-line no-restricted-globals
+  const captureModal = event.target.parentElement.id - 1;
+  // eslint-disable-next-line no-console
+  console.log(captureModal);
+
+  // eslint-disable-next-line no-restricted-globals
+  // eslint-disable-next-line no-empty
+  // eslint-disable-next-line no-restricted-globals
+  if (event.target.parentElement.getAttribute('name') === 'LoL') {
+    document.getElementById('miModal').classList.remove('hidden');
+    document.getElementById('caracteristica').innerHTML = `
+                            <img src="${newData[captureModal].image}"/>
+                            <div class="infoModal">
+                            <h2>${newData[captureModal].name}</h2>
+                            <p>${newData[captureModal].blurb}</p>
+                            <h4>defense: ${newData[captureModal].defense}</h4>
+                            <p class="paragraphModal">magic: ${newData[captureModal].magic}</p>
+                            <p class="paragraphModal">difficulty: ${newData[captureModal].difficulty}</p>
+                            <p class="paragraphModal">tag1: ${newData[captureModal].tag1}</p>
+                            <p class="paragraphModal">tag2: ${newData[captureModal].tags2}</p>
+                            <p class="paragraphModal">levelLife: ${newData[captureModal].levelLife}</p>
+                            <p class="paragraphModal">moveSpeed: ${newData[captureModal].moveSpeed}</p>
+                            <p class="paragraphModal">armor: ${newData[captureModal].armor}</p>
+                            <p class="paragraphModal">spellblock: ${newData[captureModal].spellblock}</p>
+                            <p class="paragraphModal">stats4: ${newData[captureModal].stats4}</p>
+                            <p class="paragraphModal">stats5: ${newData[captureModal].stats5}</p>
+                            <p class="paragraphModal">stats6: ${newData[captureModal].stats6}</p>
+                            </div>`;
+  }
+  // let modalOfThePlayer = '';
+  // const miModalofPlayer = (elementModal) => {
+  //   elementModal.forEach((elementForModal) => {
+  //   });
+  //   return modalOfThePlayer;
+  // };
+  // miModal.innerHTML = miModalofPlayer(newData);
+});
+
+document.querySelector('#filtered').addEventListener('click', () => {
+  document.getElementById('filtersTypeofPlayer').classList.remove('hidden');
+});
+
+document.querySelector('#filtersType').addEventListener('click', () => {
+  const championToFilter = filterType(newData, type.value);
+  root.innerHTML = roamData(championToFilter);
+
+  if (type.value !== '' && powers.value === 'falling') {
+    const powersFilterFalling = championsInFallingPower(championToFilter, powers.value);
+
+    root.innerHTML = roamData(powersFilterFalling);
+  }
+  if (type.value !== '' && powers.value === 'upward') {
+    const powersFilterUpward = championsInUpwardPower(championToFilter, powers.value);
+    root.innerHTML = roamData(powersFilterUpward);
+  }
+  if (type.value !== '' && utility.value === 'falling') {
+    const utilityFilterFalling = championsInFallingUtility(championToFilter, utility.value);
+    root.innerHTML = roamData(utilityFilterFalling);
+  }
+  if (type.value !== '' && utility.value === 'upward') {
+    const utilityFilterUpward = championsInUpwardUtility(championToFilter, utility.value);
+    root.innerHTML = roamData(utilityFilterUpward);
+  }
+  if (type.value !== '' && weakness.value === 'falling') {
+    const weaknessFilterFalling = championsInFallingDifficult(championToFilter, weakness.value);
+    root.innerHTML = roamData(weaknessFilterFalling);
+  }
+  if (type.value !== '' && weakness.value === 'upward') {
+    const weaknessFilterUpward = championsInUpwardDifficult(championToFilter, weakness.value);
+    root.innerHTML = roamData(weaknessFilterUpward);
+  }
+});
+
+document.querySelector('#searchTexto').addEventListener('keyup', () => {
+  let findChampion = [];
+  // console.log('prueba2', textToSearch.value);
+
+  findChampion = searchWord(newData, textToSearch.value);
+  // console.log('prueba3', findChampion);
+
+  root.innerHTML = roamData(findChampion);
+});
+
+document.querySelector('#home').addEventListener('click', () => {
+  window.history.go(0);
+});
+
+document.querySelector('#goBack').addEventListener('click', () => {
+  window.history.go(0);
+  document.getElementById('filtersTypeofPlayer').classList.remove('hidden');
+});
